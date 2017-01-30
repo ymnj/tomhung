c8bac834fbee2396f5a6fa8f216fc088cec9eff7
Tom <tomhung87@gmail.com>
2017-01-29 22:49:53 -0800

    Add logos and favicon assets. Add initial styles to header and main content.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,15 +4,17 @@
 	    <div class="mdl-layout__header-row">
 	      <!-- Title -->
 	      <span class="mdl-layout-title mdl-color-text--grey-900">
-	      <img id="logo" src="../../src/assets/logo.png">	TOM HUNG
+	      	<router-link class="mdl-navigation__link mdl-color-text--grey-900" to="/" exact>
+						<img id="logo" src="../../src/assets/TH02.png">
+	      	</router-link>
 	      </span>
 	      <!-- Add spacer, to align navigation to the right -->
 	      <div class="mdl-layout-spacer"></div>
 	      <!-- Navigation -->
 	      <nav class="mdl-navigation">
-	        <router-link class="mdl-navigation__link mdl-color-text--grey-900" to="/">Home</router-link>
-	       	<router-link class="mdl-navigation__link mdl-color-text--grey-900" to="/photos">Photos</router-link>
-	        <router-link class="mdl-navigation__link mdl-color-text--grey-900" to="/portfolio">Portfolio</router-link>
+	        <router-link class="mdl-navigation__link" to="/" active-class="is-active" exact>Home</router-link>
+	       	<router-link class="mdl-navigation__link" active-class="is-active" to="/photos">Photos</router-link>
+	        <router-link class="mdl-navigation__link" active-class="is-active" to="/portfolio">Portfolio</router-link>
 	      </nav>
 	    </div>
 	  </header>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div id="home-container">
 		<h1>This is HOME</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi vero molestiae accusamus. Mollitia tempora alias ea voluptate delectus dolorum, distinctio saepe explicabo laborum similique, illum deserunt soluta ipsum? Perspiciatis, facere!</p>
 	</div>
