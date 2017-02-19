ec4e6f5ac3e2cc2f3bec9cd0692e94b10bba388d
Tom <tomhung87@gmail.com>
2017-02-19 00:34:09 -0800

    Album selection is now rendered through dynamic components. Fix issue with cover-image in Album not rendering on initial re-route from Photos. Remove GET request to Cloudinary API since image URIs are hardcoded into album template components.

--- src/components/album/Tofino.vue
+++ src/components/album/Tofino.vue
@@ -1,9 +1,41 @@
 <template>
 	<div class="image-flex-wrap">
-		<div class="image-cell">
+		<div class="image-large">
 			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-1.jpg`" lazy="loading">
 		</div>
-	</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-2.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-3.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-4.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-5.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-6.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-7.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-8.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-9.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-10.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-11.jpg`" lazy="loading">
+		</div>
+		<div class="image-small">
+			<img v-lazy="`http://res.cloudinary.com/tomhung/image/upload/q_10/v1487481481/tofino/tofino-12.jpg`" lazy="loading">
+		</div>
 </template>
 
 <style lang="scss">
