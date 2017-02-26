b59af185f804247a5f2c6eecc666bb1fbe1fd0f8
Tom <tomhung87@gmail.com>
2017-02-25 22:09:31 -0800

    Add animation and scrollfire triggers to projects.

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -3,59 +3,76 @@
 		<header>
 			<h1>Code</h1>
 			<p>
-			Here is a selection of some of the projects I have worked on over the past year. If you'd like to see more, please check out my Github.</p>
+			Here is a selection of some of the projects I have worked on over the past year. If you'd like to see more, please check out my Github.Here is a selection of some of the projects I have worked on over the past year. If you'd like to see more, please check out my Github.</p>
 		</header>
 
 		<div class="project-wrap">
-			<div class="project-image">
+			<div id="peregrine-screen" class="project-image">
 				<img src="../assets/peregrine-screen.png">
 			</div>
-			<div class="project-image">
+			<div class="project-info">
+				<h2>Peregrine</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+		<div class="project-wrap project-two">
+			<div id="codrlogue-screen" class="project-image">
 				<img src="../assets/peregrine-screen.png">
 			</div>
-			<div class="project-image">
+			<div class="project-info">
+				<h2>Codrlogue</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+		<div class="project-wrap project-three">
+			<div id="blog-screen" class="project-image">
 				<img src="../assets/peregrine-screen.png">
 			</div>
-			<div class="project-image">
-				<img src="../assets/peregrine-screen.png">
+			<div class="project-info">
+				<h2>Rails Blog</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
-			<div class="project-image">
-				<img src="../assets/peregrine-screen.png">
-			</div>
-			<div class="project-image">
-				<img src="../assets/peregrine-screen.png">
-			</div>
-			<div class="project-image">
-				<img src="../assets/peregrine-screen.png">
-			</div>
-			<div class="project-image2">
-				<img src="../assets/peregrine-screen.png">
-			</div>
-
-			<!-- <div class="project-info">
-				
-			</div> -->
 		</div>
 	</div>
 </template>
 
 <script>
-
-var options = [
-      {selector: '.project-image2', offset: 50, callback: function(el) {
-        Materialize.fadeInImage($(el));
+	var options = [
+      {selector: '#peregrine-screen', offset: 700, callback: function(el) {
+        let fadeInEl = document.getElementById('codrlogue-screen');
+        fadeInEl.className += " animated fadeInLeft";
+        el.nextElementSibling.className += " animated fadeInRight";
       } },
-      {selector: '#staggered-test', offset: 205, callback: function(el) {
-        Materialize.toast("Please continue scrolling!", 1500 );
-      } },
-      {selector: '#staggered-test', offset: 400, callback: function(el) {
-        Materialize.showStaggeredList($(el));
-      } },
-      {selector: '#image-test', offset: 500, callback: function(el) {
-        Materialize.fadeInImage($(el));
+      {selector: '#code-container', offset: 1800, callback: function(el) {
+        let fadeInEl = document.getElementById('blog-screen');
+        fadeInEl.className += " animated fadeInLeft";
       } }
     ];
-    Materialize.scrollFire(options);
+  Materialize.scrollFire(options);
+
+ 	export default {
+ 		mounted() {
+ 			let el = document.getElementById('peregrine-screen');
+ 			el.className += " animated fadeInLeft";
+ 			el.nextElementSibling.className += " animated fadeInUp";
+ 		}
+ 	}
+
 
 </script>
 
@@ -69,7 +86,7 @@ var options = [
 		padding: 100px 20% 0;
 		background-color: #fff;
 		height: 100%;
-
+	
 		header{
 			background-color: #fff;
 			h1 {
@@ -92,22 +109,30 @@ var options = [
 		.project-wrap {
 			margin-bottom: 10%;
 			display: flex;
-			flex-direction: column;
 
-			img{
+			.project-image {
 				border-radius: 20px;
+				opacity: 0;
 				display: block;
 				width: 600px;
-				height: 460px;
-				box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.49);
-		    -moz-box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.49);
-		    -webkit-box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.49);
+				height: auto;
+				// height: 375px;
+				box-shadow: 4px 4px 25px rgba(0, 0, 0, 0.30);
+		    -moz-box-shadow: 4px 4px 25px rgba(0, 0, 0, 0.30);
+		    -webkit-box-shadow: 4px 4px 25px rgba(0, 0, 0, 0.30);
+				
+				img{
+					border-radius: 20px;
+					width: 100%;
+					height: auto;
+				}
 			}
-		
+
 
 			.project-info {
 				width: 40%;
-				background-color: red;
+				opacity: 0;
+				padding-left: 2.5%;
 			}
 		}
 
